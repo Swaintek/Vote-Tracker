6b7bf80c3f82b1385e23f862722e721c89b1e55b
codesnare1 <kaiduku@hotmail.com>
2015-10-14 09:23:05 -0700

    Added new images, updated README.md.  Modified app.js to include voting tracker.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Iconic hairstyle voting tracker.
 Included "click" button functionality as well as random selection of images
 
 Create user-story document.
+
+10/13/15
+Added new pictures to img folder
+Modified app.js to include a while loop(to prevent identical random pictures 
+from appearing on page.  Also, included a voting tracker.
--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-var picArr = [];
-    picArr.push('img/kid.jpg', 'img/afro 2.jpg','img/diana-ross-afro.jpg', 'img/mullet.jpg', 'img/Farrah_Fawcett.jpg', 'img/pompador.jpg','img/rachel.jpeg');
-  
+//Declare a picture array to store images
+var picArr = ['img/kid.jpg', 'img/afro 2.jpg','img/diana-ross-afro.jpg', 'img/mullet.jpg', 'img/Farrah_Fawcett.jpg', 'img/pompador.jpg','img/rachel.jpeg']
+//Declare empty picture array that will store results from the for loop
+var pictures = [];
+//Declare a function to track score for selected image
+var Pic = function (imgsrc){
+  this.imgsrc = imgsrc;  //properties of the object
+  this.score = 0;
+};
+
+for( var i=0; i<picArr.length; i++){
+  var newPic = new Pic(picArr[i]);  //Incremented score of the selected image
+  // is stored in the newPic variable
+  pictures.push(newPic); //Populate the picture array with result
+}
+
+var randomIndex1; //Create two random numbers to be used as indices in picArr
+var randomIndex2;
+
 //calculate a random number that corresponds to a random image in the array
-  function getRandomImage() {
-    var num = Math.floor(Math.random() * picArr.length);
-    return picArr[num];
-  }
+function getRandomNum() {
+  return Math.floor(Math.random() * picArr.length);
+}
 
-
+//
 function addImages() {
   var pic1 = document.getElementById('pic1');
   var pic2 = document.getElementById('pic2');
@@ -18,10 +33,10 @@ function addImages() {
     pic1.removeChild(pic1.firstChild);
   }
 
-
+  randomIndex1 = getRandomNum();
   var photo1 = document.createElement('img');
-  photo1.setAttribute('src', getRandomImage());
-  pic1.appendChild(photo1);
+  photo1.setAttribute('src', picArr[randomIndex1]);
+  pic1.appendChild(photo1);//Append img tags to elements on webpage
 
   //prevents the same picture from appearing twice.
 
@@ -29,24 +44,36 @@ function addImages() {
     pic2.removeChild(pic2.firstChild);
   }
 
+  //Created a while loop to prevent identical pictures from appearing 
+  //at the same time.
+  
+  randomIndex2 = getRandomNum();
+    while(randomIndex1 === randomIndex2){  
+      randomIndex2 = getRandomNum()
+    }
   var photo2 = document.createElement('img')
-  photo2.setAttribute('src', getRandomImage());
+  photo2.setAttribute('src', picArr[randomIndex2]);
   pic2.appendChild(photo2);
-
-  pic1.addEventListener('click', vote);
-  pic2.addEventListener('click', vote);
 }
 
-var vote = function(){
-  alert('user');
-}
-//Called the function so the page is not blank when it loads
 
-addImages();
+var pic1Button = document.getElementById('pic1');
+  pic1Button.addEventListener('click', function() {
+    pictures[randomIndex1].score += 1;  //increment the score for random image
+    console.log(picArr[randomIndex1]);  //Display result
+    addImages();
+  }
+);
+
+var pic2Button = document.getElementById('pic2');
+  pic2Button.addEventListener('click', function() {
+    pictures[randomIndex2].score += 1;
+    console.log(picArr[randomIndex2]);
+    addImages();
+  }
+);
 
 //Added button functionality for user to select image
 var picButton = document.getElementById('picButton');
 picButton.addEventListener('click', addImages);
 
-
-  
